3cf9a1b282e189122e55d8617b6d162606d2f527
BetoPita <albertopitava@gmail.com>
2021-01-25 01:36:20 -0600

    cors

--- config/server.js
+++ config/server.js
@@ -1,69 +1,79 @@
 // Servidor de Express
-const express  = require('express');
-const http     = require('http');
-const socketio = require('socket.io');
-const path     = require('path');
-const bodyParser    = require('body-parser');
-const io = require('./io');
-const Sockets  = require('./sockets');
-const cors = require('cors');
-var myApp = '';
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const path = require("path");
+const bodyParser = require("body-parser");
+const io = require("./io");
+const Sockets = require("./sockets");
+const cors = require("cors");
+var myApp = "";
+const corsOptions = {
+  credentials: true, // This is important.
+  origin: (origin, callback) => {
+    console.log("origin", origin);
+    if (typeof origin === "undefined") return callback(null, true);
+    if (whitelist.includes(origin)) return callback(null, true);
+
+    callback(new Error("Not allowed by CORS"));
+  },
+};
 class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    // Http server
+    this.server = http.createServer(this.app);
 
-    constructor() {
+    // Configuraciones de sockets
+    this.io = socketio(this.server, {
+      /* configuraciones */
+    });
+    myApp = this.io;
+  }
 
-        this.app  = express();
-        this.port = process.env.PORT;
-        // Http server
-        this.server = http.createServer( this.app );
-        
-        // Configuraciones de sockets
-        this.io = socketio( this.server, { /* configuraciones */ } );
-        myApp = this.io;
-    }
-    
-    middlewares() {
-        //Bodyparser
-        this.app.use(bodyParser.urlencoded({
-            extended: true
-        }));
-        //CORS
-        this.app.use(cors());
-        this.app.use(bodyParser.json());
-        this.app.use(express.json());
-        // Desplegar el directorio público
-        this.app.use( express.static( path.resolve( __dirname, '../public' ) ) );
-        //Cors
+  middlewares() {
+    //Bodyparser
+    this.app.use(
+      bodyParser.urlencoded({
+        extended: true,
+      })
+    );
+    //CORS
+    this.app.use(cors(corsOptions));
 
-        //EndPoints
-        //myApp = io.listen(this.io);
-        this.app.use('/api/auth/',require('../routes/auth'));
-        this.app.use('/api/mensajes/',require('../routes/mensajes'));
-    }
+    this.app.use(bodyParser.json());
+    this.app.use(express.json());
+    // Desplegar el directorio público
+    this.app.use(express.static(path.resolve(__dirname, "../public")));
+    //Cors
 
-    // Esta configuración se puede tener aquí o como propieda de clase
-    // depende mucho de lo que necesites
-    configurarSockets() {
-        new Sockets( this.io );
-        
-      //io.listen(this.io); 
-    }
+    //EndPoints
+    //myApp = io.listen(this.io);
+    this.app.use("/api/auth/", require("../routes/auth"));
+    this.app.use("/api/mensajes/", require("../routes/mensajes"));
+  }
 
-    execute() {
+  // Esta configuración se puede tener aquí o como propieda de clase
+  // depende mucho de lo que necesites
+  configurarSockets() {
+    new Sockets(this.io);
 
-        // Inicializar Middlewares
-        this.middlewares();
+    //io.listen(this.io);
+  }
 
-        // Inicializar sockets
-        this.configurarSockets();
+  execute() {
+    // Inicializar Middlewares
+    this.middlewares();
 
-        // Inicializar Server
-        this.server.listen( this.port, () => {
-            console.log('Server corriendo en puerto:', this.port );
-        });
-    }
+    // Inicializar sockets
+    this.configurarSockets();
 
+    // Inicializar Server
+    this.server.listen(this.port, () => {
+      console.log("Server corriendo en puerto:", this.port);
+    });
+  }
 }
-
 
 module.exports = Server;
